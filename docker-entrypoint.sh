7e6895d50fdeb0f82fdb1ff3dfc03198bc80e475
Jason Huddleston <huddlesj@gmail.com>
2020-05-01 15:44:30 -0500

    updated Dockerfile

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -31,11 +31,6 @@ if [ -n "${HTTP_PORT}" ]; then
   sed -i "s/80/${HTTP_PORT}/g"  /etc/nginx/conf.d/default.conf
 fi
 
-if [ -n "${HTTPS_PORT}" ]; then
-  echo "Replacing default HTTPS port (443) with the value specified by the user - (HTTPS_PORT: ${HTTPS_PORT})."
-  sed -i "s/443/${HTTPS_PORT}/g"  /etc/nginx/conf.d/default.conf
-fi
-
 
 # Execute the command specified as CMD in Dockerfile:
 exec "$@"
